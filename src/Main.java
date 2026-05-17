2af8a3fb2055aba68f225385998d27a60401e04f
matvey <mv20062017@gmail.com>
2026-05-17 22:40:40 +0300

    add hello massage

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
 
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
     }
+
 }
